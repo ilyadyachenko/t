2ff59297784d98a7ee9aa8dcdb00600439c524ad
ilyadyachenko <32840553+ilyadyachenko@users.noreply.github.com>
2018-07-10 13:52:08 +0200

    Update querytosql.php

--- querytosql.php
+++ querytosql.php
@@ -1,20 +1,19 @@
 <?php
+
 \DB::connection()->enableQueryLog();
-
-$all_offers = Offer::params()->get();
-
+Offer::params()->get();
 $queries = \DB::getQueryLog();
 
 $sql = logger($queries);
-protected static function logger($queries) 
+protected static function logger($queries)
 {
-			$formattedQueries = [];
-			foreach( $queries as $query ) :
-				$prep = $query['query'];
-				foreach( $query['bindings'] as $binding ) :
-					$prep = preg_replace("#\?#", is_numeric($binding) ? $binding : "'" . $binding . "'", $prep, 1);
-				endforeach;
-				$formattedQueries[] = $prep;
-			endforeach;
-			return $formattedQueries;
-		}
+	$formattedQueries = [];
+	foreach ($queries as $query) :
+		$prep = $query['query'];
+		foreach ($query['bindings'] as $binding) :
+			$prep = preg_replace("#\?#", is_numeric($binding) ? $binding : "'" . $binding . "'", $prep, 1);
+		endforeach;
+		$formattedQueries[] = $prep;
+	endforeach;
+	return $formattedQueries;
+}
